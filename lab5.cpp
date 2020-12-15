cb54f88ae3a0ff6db58d017e50a321ed405cefe6
Mike Zhukov <darthpigrum@gmail.com>
2020-12-15 04:22:47 +0200

    Correct variant

--- lab5.cpp
+++ lab5.cpp
@@ -3,22 +3,30 @@
 #include <iostream>
 
 auto original() {
-    std::array<std::array<int, 100>, 100> b;
+    std::array<std::array<int, 100>, 100> a;
     auto start = std::chrono::high_resolution_clock::now();
-    for(int i = 0; i < 1000; ++i)
-        for (int j = 0; j < 100; ++j)
-            for (int k = 0; k < 100; ++k)
-                ++b[k][j];
+    for(int n = 0; n < 1000; ++n) {
+        for (int i = 0; i < 100; ++i)
+            for (int j = 0; j < 100; ++j)
+                a[j][i] = a[j-1][i-1] + 1;
+        for (int i = 0; i < 100; ++i)
+            for (int j = 0; j < 100; ++j)
+                a[j][i]++;
+    }
     return duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count();
 }
 
 auto optimized() {
-    std::array<int, 10000> b;
+    std::array<int, 10000> a;
     auto start = std::chrono::high_resolution_clock::now();
-    for(int i = 0; i < 1000; ++i)
-        for (int j = 0; j < 100; ++j)
-            for (int k = 0; k < 100; ++k)
-                ++b[k + 100*j];
+    for(int n = 0; n < 1000; ++n) {
+        for (int i = 0; i < 100; ++i)
+            for (int j = 0; j < 100; ++j)
+                a[j + 100*i] = a[(j-1) + 100*(i-1)] + 1;
+        for (int i = 0; i < 100; ++i)
+            for (int j = 0; j < 100; ++j)
+                a[j + 100*i]++;
+    }
     return duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count();
 }
  
